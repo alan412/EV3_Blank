a32e0426e2f1ebcab70756c7dca4f53698c5d8bf
Alan Smith <alan@randomsmiths.com>
2016-10-16 20:30:40 -0400

    Changed to newer makeBin that no longer includes scripts in it

--- makeBin.sh
+++ makeBin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+python generateProject.py
 result=${PWD##*/}
 EV3=".ev3"
 fn=$result$EV3
 rm ../$fn
-zip ../$fn -x"README.md" -x"*.ev3" -x"*.svg" *
+zip ../$fn -x"README.md" -x"*.ev3" -x"*.svg" -x"*.py" -x"*.sh" *
 cp ../$fn  robot.ev3
